9db34547230d933dc3d83cedc6d18eb2c4c26dd3
unknown <ville.utriainen@edu.keuda.fi>
2020-10-05 08:43:04 +0300

    perjantaitehtävä

---
+++ Perjantai2108testi/perjantai2108.js
@@ -0,0 +1,11 @@
+function nappi()
+ {
+  var laske = parseInt(document.getElementById('numero'));
+  if (laske<=0)
+   {
+  document.write("Antamasi numero on positiivinen");
+  }
+  else {
+    document.write("Antamasi numero on negatiivinen")
+  }
+}
